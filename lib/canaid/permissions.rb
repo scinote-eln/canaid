a0953fb3db543a4fddcea4994045fbdc6b37af33
Luka Murn <luka.murn@biosistemika.com>
2018-03-07 14:32:19 +0100

    Use Docile gem

--- lib/canaid/permissions.rb
+++ lib/canaid/permissions.rb
@@ -1,27 +1,19 @@
 ##
 # Main entry point for registering permissions.
-
 module Canaid
   module Permissions
+    require 'docile'
+
     def self.register(&block)
-      factory = Canaid::Factories::Factory.new
-      if block_given?
-        factory.instance_eval(&block)
-      end
+      Docile.dsl_eval(Canaid::Factories::Factory.new, &block)
     end
 
     def self.register_generic(&block)
-      factory = Canaid::Factories::FactoryGeneric.new
-      if block_given?
-        factory.instance_eval(&block)
-      end
+      Docile.dsl_eval(Canaid::Factories::FactoryGeneric.new, &block)
     end
 
     def self.register_for(obj_class, &block)
-      factory = Canaid::Factories::FactoryForObjClass.new(obj_class)
-      if block_given?
-        factory.instance_eval(&block)
-      end
+      Docile.dsl_eval(Canaid::Factories::FactoryForObjClass.new(obj_class), &block)
     end
   end
 end
